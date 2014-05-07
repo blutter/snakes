307a78a1e63dadb9e94e430a729ccdb5d2d00b2c
blt <blt@vsl.com.au>
2014-05-07 16:26:02 +1000

    Rename snakes to snake

--- snakes/app/controllers/snakes.js
+++ snakes/app/controllers/snakes.js
@@ -1,17 +1,17 @@
 var snakeApp = angular.module('snakeApp', ['SnakeModel', 'ngTouch']);
 
 
-// Index: http://localhost/views/snake/index.html
+// Index: http://localhost/views/snakes/index.html
 
 snakeApp.controller('IndexCtrl', function ($scope, SnakeRestangular) {
 
   // Helper function for opening new webviews
   $scope.open = function(id) {
-    webView = new steroids.views.WebView("/views/snake/show.html?id="+id);
+    webView = new steroids.views.WebView("/views/snakes/show.html?id="+id);
     steroids.layers.push(webView);
   };
 
-  // Fetch all objects from the local JSON (see app/models/snake.js)
+  // Fetch all objects from the local JSON (see app/models/snakes.js)
   SnakeRestangular.all('snake').getList().then( function(snakes) {
     $scope.snakes = snakes;
   });
@@ -23,11 +23,11 @@ snakeApp.controller('IndexCtrl', function ($scope, SnakeRestangular) {
 });
 
 
-// Show: http://localhost/views/snake/show.html?id=<id>
+// Show: http://localhost/views/snakes/show.html?id=<id>
 
 snakeApp.controller('ShowCtrl', function ($scope, $filter, SnakeRestangular) {
 
-  // Fetch all objects from the local JSON (see app/models/snake.js)
+  // Fetch all objects from the local JSON (see app/models/snakes.js)
   SnakeRestangular.all('snake').getList().then( function(snakes) {
     // Then select the one based on the view's id query parameter
     $scope.snake = $filter('filter')(snakes, {id: steroids.view.params['id']})[0];
