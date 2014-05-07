b4c9f791262d10c7f0dd2c6ccec410b8ad844070
blt <blt@vsl.com.au>
2014-05-07 15:51:10 +1000

    Added example tests

---
+++ snakes/app/tests/example.js
@@ -0,0 +1,12 @@
+/**
+ * Created by blt on 7/05/2014.
+ */
+describe("Example test", function() {
+	it("basic test", function() {
+		expect(true).toBe(true);
+	});
+
+	it("basic test 2", function() {
+		expect(true).toBe(false);
+	});
+});
