5205b1e11c0b5325b6cd2031bcd3511576b0efd2
blt <blt@vsl.com.au>
2014-05-27 17:57:43 +1000

    Fix bower dependencies

--- snakes/app/models/snakes.js
+++ snakes/app/models/snakes.js
@@ -15,7 +15,6 @@ module.factory('SnakeRestangular', function(Restangular) {
   return Restangular.withConfig(function(RestangularConfigurer) {
 
     RestangularConfigurer.setBaseUrl('http://10.10.254.68/snakes/snakes');
-//    RestangularConfigurer.setRequestSuffix('.json');
     RestangularConfigurer.setRestangularFields({
       id: "snake_id"
     });
